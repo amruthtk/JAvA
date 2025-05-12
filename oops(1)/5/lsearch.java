946438815e9c4df94361cbe196d17f492de19591
amruthtk <69493048+amruthtk@users.noreply.github.com>
2025-05-12 15:00:15 +0530

    Update Linear.java

--- oops(1)/5/lsearch.java
+++ oops(1)/5/lsearch.java
@@ -1,33 +1,34 @@
 import java.util.Scanner;
-import java.util.Arrays;
 
-public class lsearch {
-	void define(int arr[],int n,int item)
-    {
-        int i,j;
-        for(i=0;i<n;i++)
-        {
-          if(arr[i]==item)
-          {
-          System.out.println("FOUND");
-          }
-          }
-          System.out.println("NOT FOUND");
-          }
-  public static void main (String args[]){
-  int i,item,n;
-  Scanner sc=new Scanner(System.in);
-        lsearch lin= new lsearch();
-        System.out.println("Enter the number of Elments :");
-        n=sc.nextInt();
-        int[] arr =new int[n];
-        System.out.println("Enter "+n+" Elements :");
-        for(i=0;i<n;i++)
-        {
-            arr[i]=sc.nextInt();
+class Linear {
+    void define(int arr[], int n, int item) {
+        int i;
+        for (i = 0; i < n; i++) {
+            if (arr[i] == item) {
+                System.out.println("Item found at position: " + (i + 1));
+                return;
+            }
         }
-        System.out.println("Enter the number to be searched : ");
-        item=sc.nextInt();
-        lin.define(arr,n,item);
+        System.out.println("Item not found");
+    }
+
+    public static void main(String args[]) {
+        int n, item, i;
+        Scanner sc = new Scanner(System.in);
+        Linear lin = new Linear();
+
+        System.out.print("Enter the number of elements: ");
+        n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " numbers: ");
+        for (i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Enter the number to be searched: ");
+        item = sc.nextInt();
+
+        lin.define(arr, n, item);
     }
 }
